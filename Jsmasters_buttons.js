805a4114b1a7710a7a55bd2b604da8d91e1d0bc1
domballs96 <94650621+domballs96@users.noreply.github.com>
2022-01-03 15:46:33 -0800

    Create Jsmasters_buttons.js

---
+++ Jsmasters_buttons.js
@@ -0,0 +1,2 @@
+function nbt(content, class_, id){const body = document.body;const new_button = document.createElement("button");new_button.className=class_;new_button.id=id;new_button.innerText=content;body.append(new_button)}
+// nbt("k" , "k" , "k") deff nbt() or new_button() has 3 aruguments : 1. Content {name} : 2. Class {For Css} 3. id {to use document.getElmentById()} Content is the only reqwired arugument
