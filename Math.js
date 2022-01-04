a0ea2715e774153e466ea337856c477d83552080
domballs96 <94650621+domballs96@users.noreply.github.com>
2022-01-03 18:42:03 -0800

    Create Math.js

---
+++ Math.js
@@ -0,0 +1,5 @@
+// math
+function add(num1, num2){ return num1 + num2 } // adddtion
+function minus(num1, num2){ return num1 - num2 } // subtraction
+function mut(num1, num2){ return num1 * num2 } // multiplication
+function divi(num1, num2){ return num1 / num2 } // divide
