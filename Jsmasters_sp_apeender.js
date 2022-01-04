5fe96ed47b9de2d5d6f910aab0b941e339cb1160
domballs96 <94650621+domballs96@users.noreply.github.com>
2022-01-03 17:55:54 -0800

    Create Jsmasters_sp_apeender.js

---
+++ Jsmasters_sp_apeender.js
@@ -0,0 +1,4 @@
+function ap(what_elm){const body = document.body; body.append(what_elm)} // appends child to body
+function apc(what_elm){const body = document.body; body.appendChild(what_elm)} // appends child to body
+// if you read it it allredy makes the body for the DOM 
+// it really just makes "const body = document.body;append("new_div")" to ap() or "const body = document.body;appendChild("new_div")" to apc()
