cad54a7f157c88c76689703d05da49fb80cd18c8
domballs96 <94650621+domballs96@users.noreply.github.com>
2022-01-03 18:40:49 -0800

    Create window_bger_obj.js

---
+++ window_bger_obj.js
@@ -0,0 +1,5 @@
+// window obj
+function ask(asking){window.prompt(asking)} // 1
+function prompt(asking){window.prompt(asking)} // 1
+function say(saying){window.alert(saying)} // 2
+function inform(saying){window.alert(saying)} // 2
